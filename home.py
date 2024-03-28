7063fd4ead1dee37dbc65771d2d61766b203211f
PK006 <plai6588@gmail.com>
2024-03-28 14:33:54 +0700

    .

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.page_link("Home.py", label="หน้าแรก", icon="🏠")
+st.page_link("home.py", label="หน้าแรก", icon="🏠")
 
 st.page_link("pages/Statistic.py", label="การนำเสนอข้อมูลด้วยสถิติ", icon="1️⃣")
 st.page_link("pages/Chart.py", label="การทำนายข้อมูลด้วยการจินตทัศน์ข้อมูล", icon="2️⃣", disabled=False)
